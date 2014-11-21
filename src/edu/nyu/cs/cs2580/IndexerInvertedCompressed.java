ae6a03df6b9538bed68b01cfc77363e305619369
Suraj Swaminathan <ss7359@energon1.cims.nyu.edu>
2014-11-21 01:30:14 -0500

    LowerCase

--- src/edu/nyu/cs/cs2580/IndexerInvertedCompressed.java
+++ src/edu/nyu/cs/cs2580/IndexerInvertedCompressed.java
@@ -279,12 +279,12 @@ public class IndexerInvertedCompressed extends Indexer implements Serializable {
 	// create the document
 	DocumentIndexed doc = new DocumentIndexed(_documents.size());
 	doc.setTitle(title);
-	doc.setNumViews(((LogMinerNumviews)_logMiner).getNumviews(title.toLowerCase()));
-	doc.setPageRank( ( (CorpusAnalyzerPagerank )_corpusAnalyzer).getPagerank(title));
+	doc.setNumViews( ( (LogMinerNumviews)_logMiner).getNumviews(title.toLowerCase()));
+	doc.setPageRank( ( (CorpusAnalyzerPagerank )_corpusAnalyzer).getPagerank(title.toLowerCase()));
 	doc.setUrl(url);
 	((DocumentIndexed) doc).removeAll();
 	//if (( (CorpusAnalyzerPagerank )_corpusAnalyzer).getPagerank(title) > 0)
-	System.out.println(( (LogMinerNumviews) _logMiner).getNumviews(title));
+	System.out.println(( (LogMinerNumviews) _logMiner).getNumviews(title.toLowerCase()));
 
 	// add the document
 	_documents.add(doc); 
