fc9a92a46817e8769509c58a4705bf28063e7522
Suraj2991 <suraj2991@gmail.com>
2014-11-21 17:56:27 -0800

    Created classes for PRF

---
+++ src/edu/nyu/cs/cs2580/Terms.java
@@ -0,0 +1,13 @@
+package edu.nyu.cs.cs2580;
+
+public class Terms {
+	private String _name = "";
+	
+	public void setName(String name) {
+	    this._name = name;
+	  }
+	public String getName(){
+		return this._name;
+	}
+
+}
