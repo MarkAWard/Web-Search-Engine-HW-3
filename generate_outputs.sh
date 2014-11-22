14cb730918c5a074ae592205db0d474f92f95861
Arpit Jain <arpit.jain2811@gmail.com>
2014-11-22 17:41:11 -0500

    Updated as per HW3 grading

---
+++ generate_outputs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PRF=./prf/
+OUTPUT=qsim.tsv
+PREFIX=prf-
+RANKER="conjunctive"
+SUFFIX=.tsv
+PRFFILE=prf.tsv
+
+rm -rf $PRF
+mkdir $PRF
+rm $OUTPUT
+rm -f prf*.tsv
+i=0
+while read q ; do
+	i=$((i + 1));
+	FILE=$PRF$PREFIX$i$SUFFIX;
+	if [ ! -e $FILE ]
+	then
+    	echo "Creating file... $FILE"
+    	touch $FILE
+	fi
+	curl "http://localhost:25808/prf?query=$q&ranker=$RANKER&numdocs=10&numterms=5" >> $FILE
+    if [ ! -e $PRFFILE ]
+    then
+        echo "Creating file... $PRFFILE"
+        touch $PRFFILE
+    fi
+    echo $q:$FILE >> $PRFFILE
+    done < queries.tsv
+java -classpath "src:library/*" -Xmx512m edu.nyu.cs.cs2580.Bhattacharyya $PRFFILE $OUTPUT
+    	
